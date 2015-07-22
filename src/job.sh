f9199edab80b142776e3943280dca72774f23dac
dustyfresh <dustin.warren1337@gmail.com>
2015-07-22 01:01:12 -0700

    meh

--- src/job.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-guide="/opt/deepdream-api/doge.jpg"
-input_dir="$1"
-output_dir="$2"
-for i in $(ls $input_dir); do
-	/opt/deepdream-api/src/dream.py $guide $input_dir/$i $output_dir/$i;
-done
