8e1619b94657a73cf5380d3375252b31caa24ebb
dustyfresh <dustin.warren1337@gmail.com>
2015-07-22 17:38:46 -0700

    worked on job script....it's pretty ghetto

--- src/job.php
+++ src/job.php
@@ -1,10 +1,24 @@
 #!/usr/bin/php
 <?php
+set_time_limit(0);
+$guide = '/home/ubuntu/images/doge.jpg'; // temporary guide until I factor it in as an API argument
 require_once 'api/inc/db.php';
 $db = $m->deepdreamapi;
 $collection = $db->dreams;
 
 $cursor = $collection->find(array(), array('_id' => 0));
-foreach($cursor as $result){
-	print_r($result);
+foreach($cursor as $results){
+	foreach($results as $result){
+		$dream_id = $result['dream_id'];
+		$file_ext = escapeshellarg($result['file_type']);
+		$input = escapeshellarg($result['file_name']);
+		$output = '/opt/deepdream-api/dream/' . $dream_id . "." . $file_ext;
+		if(!file_exists($output)){
+			print "Dreaming $dream_id\n";
+			shell_exec("/usr/bin/python /opt/deepdream-api/src/dream.py $guide $input $output 2>/dev/null &");
+		}
+	}
 }
+
+
+
