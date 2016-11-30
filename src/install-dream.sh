30bc0595f875a95e1b3e9137dd2991de7624a5d2
crazyjaco <crazyjac0@gmail.com>
2016-11-29 20:28:08 -0500

    Removed gcc stuff from apt-get install statement and commented out the update-alternatives lines since they all use an out of date compiler.

--- src/install-dream.sh
+++ src/install-dream.sh
@@ -9,11 +9,11 @@ apt-get -y upgrade
 apt-get -y install linux-source
 apt-get -y install linux-headers-`uname -r`
 printf "export PYTHONPATH=$PATH:/opt/caffe/python\nexport PATH=$PATH:/opt/caffe/.build_release/tools" >> /etc/profile
-apt-get update && sudo apt-get install -y bc cmake curl gcc-4.6 g++-4.6 gcc-4.6-multilib g++-4.6-multilib gfortran git libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev liblmdb-dev libjpeg62 libfreeimage-dev libatlas-base-dev pkgconf protobuf-compiler python-dev python-pip unzip wget python-numpy python-scipy python-pandas python-sympy python-nose
-update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-4.6 30
-update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-4.6 30
-update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 30
-update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 30
+apt-get update && sudo apt-get install -y bc cmake curl gfortran git libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libboost-all-dev libhdf5-serial-dev liblmdb-dev libjpeg62 libfreeimage-dev libatlas-base-dev pkgconf protobuf-compiler python-dev python-pip unzip wget python-numpy python-scipy python-pandas python-sympy python-nose
+# update-alternatives --install /usr/bin/cc cc /usr/bin/gcc-4.6 30
+# update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++-4.6 30
+# update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.6 30
+# update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.6 30
 cd /opt && git clone https://github.com/BVLC/caffe.git
 cd /opt && wget https://google-glog.googlecode.com/files/glog-0.3.3.tar.gz && tar zxvf glog-0.3.3.tar.gz && cd /opt/glog-0.3.3 && ./configure && make && make install
 cd /opt && wget https://github.com/schuhschuh/gflags/archive/master.zip && unzip master.zip && cd /opt/gflags-master && mkdir build && cd /opt/gflags-master/build && export CXXFLAGS="-fPIC" && cmake .. && make VERBOSE=1 && make && make install
